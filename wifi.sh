dcbecd599ad0bf9f4e0f668d7d908721af17d045
John <30934921+jziyangli@users.noreply.github.com>
2018-08-07 12:13:16 -0700

    Add files via upload

---
+++ wifi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "%USERPROFILE%\\Desktop" & for /f "tokens=2 delims=:"  %a in ('netsh wlan show profile ^| findstr "Profile"') do echo %a>>a
+
+for /f "tokens=1 delims= "  %a in (a) do echo %a>>b
+
+del a & for /f "tokens=*"  %a in (b) do (echo %a:>>log & netsh wlan show profiles "%a"  key=clear|findstr "Key Content">>log)
+
+del b & notepad log & timeout 3 & del log & exit
